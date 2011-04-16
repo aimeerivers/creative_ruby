ae845739bde6c5e9179a312fae11596f63395bb7
aimee daniells <aimee@edendevelopment.co.uk>
2011-04-16 23:59:27 +0100

    Can now use the wrapper and lose the inheritance.

--- music/lib/random_music_generator.rb
+++ music/lib/random_music_generator.rb
@@ -1,14 +1,19 @@
 require 'live_midi'
+require 'live_midi_wrapper'
 
-class RandomMusicGenerator < LiveMIDI
+class RandomMusicGenerator
+
+  def initialize
+    @midi = LiveMidiWrapper.new(LiveMIDI.new)
+  end
 
   def play_music
-    self.program_change(1, 1)
+    @midi.choose_instrument :piano, for_channel: 1
     while true do
       note = rand(90) + 10
       time = rand(3)
       puts "Playing note #{note} for #{time} seconds"
-      self.note_on(1, note, 100)
+      @midi.play_note note, on_channel: 1, with_velocity: 100
       sleep(time)
     end
   end
